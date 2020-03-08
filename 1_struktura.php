91d6561e3f21b2cc9920a35b93876cd162f92efa
krystianmakowski <krystian.makowski93@gmail.com>
2020-03-08 11:09:05 +0100

    struktura

---
+++ 1_struktura.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $name="Karol";
+      $surname="Kowalski"
+
+      echo "$name $surname";
+      echo $name.$surname;
+      echo $name,$surname;
+
+      //heredoc
+
+      echo <<<SHOW
+      <hr>
+        Imię: $name <br>
+        Nazwisko: $surname
+        <hr>
+SHOW;
+      $text=<<<SHOW
+      <hr>
+      Imię: $name <br>
+      Nazwisko: $surname
+      <hr>
+SHOW;
+      echo $text;
+     ?>
+  </body>
+</html>
