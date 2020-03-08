df9f89bbf70befdfe04763837e70a6030771ed7c
krystianmakowski <krystian.makowski93@gmail.com>
2020-03-08 14:16:21 +0100

    fresh

--- 2_opracjenazmiennych.php
+++ 2_opracjenazmiennych.php
@@ -10,5 +10,44 @@ $bin=0b1011;
 echo $int;
 echo $hex;
 echo $oct;
-echo $bin;
+echo $bin,"<hr>";
+//echo phpinfo();
+//operatory bitowe
+$x=0b1010;
+echo $x,"<br>";//10
+$x=$x>>1; //101
+echo $x,"<br>";//5
+$x=$x<<2; //10100
+echo $x,"<hr>"; //20
+
+//rowne / identyczne
+$x=2;
+$y=2.0;
+if ($x==$y){
+echo "Równe<br>";
+}else{
+echo "Różne<br>";
+}
+
+if ($x===$y){
+echo "Identyczne<br>";
+}else{
+echo "Nieidentyczne<br>";
+}
+
+echo gettype($x),"<br>";//integer
+echo gettype($y),"<br>";//double
+
+$x=20;
+$y=10;
+$result=$x<=>$y;
+echo $result,"<hr>";
+
+//operatory rzutowania danych
+$text="123ssd";
+$x=(int)$text;
+echo $x,"<br>";//123
+echo gettype($text),"<br>";//string
+echo gettype($x),"<br>";//integer
+
  ?>
