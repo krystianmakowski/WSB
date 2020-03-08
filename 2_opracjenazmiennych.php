07b07c65cc272b02f925d45242668e110019ae04
krystianmakowski <krystian.makowski93@gmail.com>
2020-03-08 13:49:17 +0100

    2opercje

---
+++ 2_opracjenazmiennych.php
@@ -0,0 +1,14 @@
+<?php
+$potega=2**10;
+echo $potega,"<br>";
+//systemy liczbowe
+$int=10;
+$hex=0xA; //0xliczba
+$oct=012; //1*8^1+2*8^0=10
+$bin=0b1011;
+
+echo $int;
+echo $hex;
+echo $oct;
+echo $bin;
+ ?>
