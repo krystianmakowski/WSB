b3a4c61398b8108a536daf75cb52003903b91a0f
krystianmakowski <krystian.makowski93@gmail.com>
2020-04-05 13:38:56 +0200

    css

---
+++ css/elementy.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<p>text1</p>
+<p>text2</p>
+<a href"">link 1 </a>
+<a href"">link 2 </a>
+    <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam suscipit ex deserunt 
+    atque quibusdam omnis fugiat, tempore, officia facere fuga explicabo sit? Dolor explicabo velit hic 
+    exercitationem sapiente reprehenderit?
+    </div>
+    <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam suscipit ex deserunt 
+    atque quibusdam omnis fugiat, tempore, officia facere fuga explicabo sit? Dolor explicabo velit hic 
+    exercitationem sapiente reprehenderit?
+    </div>
+    <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam suscipit ex deserunt 
+    atque quibusdam omnis fugiat, tempore, officia facere fuga explicabo sit? Dolor explicabo velit hic 
+    exercitationem sapiente reprehenderit?
+    </div>
+</body>
+</html>
---
+++ css/elementy_1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <img src="ferrari.jpg" alt="Ferrari">
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem nam porro voluptatibus reiciendis 
+            esse alias aut quaerat a optio harum, ratione sed doloremque consectetur exercitationem eius quia.
+             Fugiat, eligendi numquam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, 
+             sapiente repellat? Ratione minima tempora suscipit sunt omnis eum, quaerat fugit quibusdam 
+             praesentium. Atque repellat vero quis, porro ad itaque totam?
+        </p>
+    </div>
+</body>
+</html>
